13ca0e6f23383b16a1547b49721f8810ddeea986
Robert Scott <code@humanleg.org.uk>
2013-05-27 14:52:39 +0100

    profiles: disable traffic signal penalty in both car and bicycle profiles

--- profiles/bicycle.lua
+++ profiles/bicycle.lua
@@ -64,7 +64,7 @@ obey_oneway 			= true
 obey_bollards 			= false
 use_restrictions 		= true
 ignore_areas 			= true -- future feature
-traffic_signal_penalty 	= 2
+traffic_signal_penalty 	= 0
 u_turn_penalty 			= 20
 
 -- End of globals
